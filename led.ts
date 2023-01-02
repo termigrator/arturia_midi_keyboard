1730c1113c3296318302d5982ca6ed1dbf7bdbec
Robert Krueger <robert1@kaider.info>
2023-01-02 12:34:30 +0100

    comments now english, getter and setter for color.

--- led.ts
+++ led.ts
@@ -1,17 +1,21 @@
 import { LEDColor } from "./arturia_constants";
 
 export class LED {
+
     ID: number; Name: string; Status: number; ColorOn: LEDColor; ColorOff: LEDColor; OnValueChanged: Function;
     constructor(id: number, color: LEDColor, onvaluechanged: Function) {
         this.ID = id;
         this.Status = 0;
         this.ColorOn = color;
         this.ColorOff = LEDColor.black;
-        this.OnValueChanged = onvaluechanged;
+        this.OnValueChanged = onvaluechanged; //This callback points to an function on the midi-Board, which will send the new color-code the board
     }
 
 
-    //beim Einschalten wird die für die Anzeige vorgeshene Farbe über das Callback im Midi-Controllergesetzt.
+    /**
+     * If nessecary, a new status is generated and the callback to change the color
+     * to the defined "on-color" on the Midi-Board is called
+     */
     on() {
         if (this.Status == 0) {
             //console.log(`new LED status 1 for LED ${this.ID}, color will be set to ${this.ColorOn}`)
@@ -21,7 +25,10 @@ export class LED {
 
     }
 
-    //beim Ausschalten wird die Farbe Schwarz über das Callback im Midi-Controller gesendet
+     /**
+     * If nessecary, a new status is generated and the callback to change the color
+     * to the defined "off-color", wich standard is mostly black,  on the Midi-Board is called
+     */
     off() {
         if (this.Status == 1) {
             this.Status = 0
@@ -29,7 +36,12 @@ export class LED {
             this.OnValueChanged(this.ID, this.ColorOff);
         }
     }
-    color(color: LEDColor) {
+
+    set color(color: LEDColor) {
         this.ColorOn = color;
+    }
+
+    get color() {
+        return this.color;
     }
 }
